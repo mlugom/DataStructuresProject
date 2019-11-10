b1c3cd6cddb99e9be8406b8abf70825992e2173f
Manuel Lugo <manuel@manuel-G6-V3>
2019-11-10 16:48:47 -0500

    Interfaz

---
+++ src/main/java/interfaz/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+/**
+ *
+ * @author Farid Estepa
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        Principal principal= new Principal();
+        principal.setVisible(true);
+        principal.setLocation(null);
+    }
+  
+    
+}
